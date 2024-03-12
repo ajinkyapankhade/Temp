8d0522cb91b19da5c6b523c4a32b1ed19be74bfe
ajinkyapankhade <ajinkyapankhade@gmail.com>
2024-03-12 16:12:58 +0530

    added one line

---
+++ mypysparkcode.py
@@ -0,0 +1,16 @@
+#accept a matrix as a input and find its tranpose
+from numpy import*
+
+r,c= [int(i) for i in input('how many rows, cols?').split(',')]
+arr = zeros((r,c),dtype = int)
+print('Enter matrix:')
+
+for i in range(r):
+    arr[i]=[int(x) for x in input().split()]
+
+m= matrix(arr)
+m1=m.transpose()
+print('matrix is \n',m)
+print('tranpose of matrix is\n',m1)
+ghaj
+
